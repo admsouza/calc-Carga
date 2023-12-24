39f993e4b72b4000f635bb1c0f4123b8200ef546
Jackson <j.admsouza@gmail.com>
2023-12-24 15:02:56 -0300

    Estilização e criação do espaço memória de calculo

--- src/Mod-Carga/Carga.js
+++ src/Mod-Carga/Carga.js
@@ -1,36 +1,33 @@
 import React, { useState } from "react";
 import TextField from "@mui/material/TextField";
 
-
 import "./Carga.css";
 
-
 const formatCurrency = (value) => {
-  return value.toLocaleString('pt-BR', {
-    style: 'currency',
-    currency: 'BRL',
+  return value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+    minimumFractionDigits: 2,
   });
 };
 
 const Carga = () => {
-  const [recebido, setRecebido] = useState(0);
-  const [resultbase, setResultBase] = useState(0);
+  const [recebido, setRecebido] = useState("");
+  const [resultbase, setResultBase] = useState("");
+
   const BaseRecebido = () => {
     const ResultB = recebido * 0.2;
     setResultBase(ResultB);
-
-
-    
   };
 
-  const [outroVinculo, setOutroVinculo] = useState(0);
-  const [resultvinculo, setResultVinculo] = useState(0);
+  const [outroVinculo, setOutroVinculo] = useState("");
+  const [resultvinculo, setResultVinculo] = useState("");
   const BaseOutrosVin = () => {
     const resultOutros = outroVinculo * 0.2;
     setResultVinculo(resultOutros);
   };
 
-  const [inssatual, setInssAtual] = useState(0);
+  const [inssatual, setInssAtual] = useState("");
   const CalcInssAt = () => {
     const inssat = resultbase * 0.11;
     if (resultbase > 7507.49) {
@@ -40,8 +37,7 @@ const Carga = () => {
     }
   };
 
-  const [inssoutros, setOutros] = useState(0);
-
+  const [inssoutros, setOutros] = useState("");
   const CalInssOutros = () => {
     const inssout = resultvinculo * 0.11;
     if (resultvinculo > 7507.49) {
@@ -51,7 +47,7 @@ const Carga = () => {
     }
   };
 
-  const [insstotal, setInssTotal] = useState(0);
+  const [insstotal, setInssTotal] = useState("");
   const CalcInssFinal = () => {
     const inssfinal = inssatual + inssoutros;
     if (inssfinal > 825.82) {
@@ -61,87 +57,133 @@ const Carga = () => {
     }
   };
 
-  const [dependentes, setDependentes] = useState(0);
+  const [dependentes, setDependentes] = useState("");
+  const [resultdependentes, setResultDependentes] = useState("");
+  const CalcDependentes = () => {
+    const resultdep = dependentes * 189.59;
+    setResultDependentes(resultdep);
+  };
 
-  const [pensao, setPensao] = useState(0);
-  const [pcomplementar, setPComplementar] = useState(0);
+  const [pensao, setPensao] = useState("");
+  const [resultpesnsao, SetResultPesnsao] = useState("");
+  const FormatPensao = () => {
+    const formatvalor = pensao * 1;
+    SetResultPesnsao(formatvalor);
+  };
+
+  const [pcomplementar, setPComplementar] = useState("");
 
   const CalcInss = () => {
     CalcInssAt();
     CalInssOutros();
+    FormatPensao();
+  };
+
+  const Format = () => {
+    CalcInssFinal(FormatPensao());
   };
 
   return (
     <div className="main">
-  
+      <h1>Calculadora - Transporte de Carga </h1>
 
-      <TextField
-        type="number"
-        label="Valor Recebido"
-        id="input"
-        onBlur={BaseRecebido}
-        value={recebido}
-        onChange={(e) => setRecebido(+e.target.value)}
-        size="large"
-        color="success"
-        required
-      />
+      <div className="box-calculadora">
+        <TextField
+          type="number"
+          label="Valor Recebido"
+          id="input"
+          onBlur={BaseRecebido}
+          value={formatCurrency(recebido)}
+          onChange={(e) => setRecebido(e.target.value)}
+          size="large"
+          color="success"
+          required
+        />
 
-      <br />
+        <br />
 
-      <TextField
-        label="Outros Vinculos"
-        type="number"
-        id="input"
-        onBlur={BaseOutrosVin}
-        value={outroVinculo}
-        onChange={(e) => setOutroVinculo(+e.target.value)}
-        color="success"
-      />
+        <TextField
+          label="Outros Vinculos"
+          type="number"
+          id="input"
+          onBlur={BaseOutrosVin}
+          value={formatCurrency(outroVinculo)}
+          onChange={(e) => setOutroVinculo(e.target.value)}
+          color="success"
+        />
 
-      <br />
+        <br />
 
-      <TextField
-        label="Dependentes "
-        type="number"
-        id="input"
-        onFocus={CalcInss}
-        value={dependentes}
-        onChange={(e) => setDependentes(+e.target.value)}
-        color="success"
-      />
-      <br />
+        <TextField
+          label="Dependentes"
+          type="number"
+          id="input"
+          onBlur={CalcDependentes}
+          value={formatCurrency(dependentes)}
+          onChange={(e) => setDependentes(e.target.value)}
+          color="success"
+        />
+        <br />
 
-      <TextField
-        label="Pensão "
-        type="number"
-        id="input"
-        value={pensao}
-        onChange={(e) => setPensao(+e.target.value)}
-        color="success"
-      />
-      <br />
+        <TextField
+          label="Pensão "
+          type="number"
+          id="input"
+          onFocus={CalcInss}
+          value={formatCurrency(pensao)}
+          onChange={(e) => setPensao(e.target.value)}
+          color="success"
+        />
+        <br />
 
-      <TextField
-        label="Prev. Complementar "
-        type="number"
-        id="input"
-        onFocus={CalcInssFinal}
-        value={pcomplementar}
-        onChange={(e) => setPComplementar(+e.target.value.formatCurrency)}
-        color="success"
-      />
-      <br />
-<div>
-      <p> Base Valor Recebido 20% = { formatCurrency (resultbase)}</p>
-      <p> Base Outros Vinculos 20% = { formatCurrency (resultvinculo)}</p>
-      <p> INSS Atual = {formatCurrency(inssatual) }</p>
-      <p> INSS Out. Vinculos = { formatCurrency (inssoutros)}</p>
-      <p> INSS Total = {formatCurrency (insstotal)}</p>
+        <TextField
+          label="Prev. Complementar "
+          type="number"
+          id="input"
+          onFocus={Format}
+          value={formatCurrency(pcomplementar)}
+          onChange={(e) => setPComplementar(e.target.value)}
+          color="success"
+        />
+        <br />
+        <div className=" box-mcalculo">
+          <p className="box-memoria"> Memória de Cálculo</p>
+          <p>
+            Base Valor Recebido 20% ={" "}
+            <span id="result">{formatCurrency(resultbase)}</span>
+          </p>
 
+          <p>
+            {" "}
+            Base Outros Vinculos 20% ={" "}
+            <span id="result">{formatCurrency(resultvinculo)}</span>
+          </p>
 
-</div>
-      
+          <p>
+            {" "}
+            INSS Atual = <span id="result">{formatCurrency(inssatual)} </span>
+          </p>
+          <p>
+            {" "}
+            INSS Out. Vinculos ={" "}
+            <span id="result">{formatCurrency(inssoutros)}</span>
+          </p>
+          <p>
+            {" "}
+            INSS Total = <span id="result">{formatCurrency(insstotal)}</span>
+          </p>
+
+          <p>
+            {" "}
+            Dependentes ={" "}
+            <span id="result">{formatCurrency(resultdependentes)}</span>
+          </p>
+          <p>
+            {" "}
+            Pensão = <span id="result">{formatCurrency(resultpesnsao)}</span>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
